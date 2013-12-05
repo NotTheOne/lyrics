cb2ffd51c81d5aded9a6b48be4052cb6d8c0d49b
Roger Camps <rogercampsny@gmail.com>
2013-12-05 06:51:04 -0500

    modified song model and deleted album model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131204163436) do
+ActiveRecord::Schema.define(version: 20131205114547) do
 
   create_table "albums", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20131204163436) do
 
   create_table "songs", force: true do |t|
     t.string   "name"
+    t.integer  "album_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
